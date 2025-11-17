3bca4847316f76aebb68b476c59ae3538ef1672a
Sujendran Reddy <ST10457761@vcconnect.edu.za>
2025-11-17 18:36:22 +0200

    Added HR super user role, added seeding for the role in program cs for app startup, created an hr user in the program.cs, configured identity in program.cs with dbcontext, enforced HR account creation if it was missing.

---
+++ CMCS/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<ApplicationUser>()
+               .Property(u => u.HourlyRate)
+               .HasPrecision(18, 2);
+    }
+}
--- CMCS/Program.cs
+++ CMCS/Program.cs
@@ -1,21 +1,32 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    await CreateHRRole(scope.ServiceProvider);
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -25,5 +36,29 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
 app.Run();
+
+async Task CreateHRRole(IServiceProvider services)
+{
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+    if (!await roleManager.RoleExistsAsync("HR"))
+        await roleManager.CreateAsync(new IdentityRole("HR"));
+
+    var hrUser = await userManager.FindByEmailAsync("hr@system.com");
+
+    if (hrUser == null)
+    {
+        hrUser = new ApplicationUser
+        {
+            UserName = "hr@system.com",
+            Email = "hr@system.com",
+            FirstName = "System",
+            LastName = "Admin"
+        };
+
+        await userManager.CreateAsync(hrUser, "Admin#1234"); // HR password
+        await userManager.AddToRoleAsync(hrUser, "HR");
+    }
+}
